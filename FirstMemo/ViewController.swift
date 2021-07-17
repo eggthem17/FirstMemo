e594cf4399479b0fcd082f31ee566a134b7ba74e
paxmiler <paxmiler@gmail.com>
2021-07-17 18:10:32 +0900

    Initial Commit

---
+++ FirstMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstMemo
+//
+//  Created by Martin.Q on 2021/07/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
